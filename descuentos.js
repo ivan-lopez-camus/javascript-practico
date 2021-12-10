27ea3d35ea7dd08bca4779530ae001948e7b907b
Ivan López <ivan.lopez1@unmsm.edu.pe>
2021-12-10 11:28:45 -0500

    Promedios

--- descuentos.js
+++ descuentos.js
@@ -27,3 +27,32 @@ function onClickButtonPrice(){
 
 }
 
+const coupons = [
+    "JuanDC_es_Batman",
+    "pero_no_le_digas_a_nadie",
+    "es_un_secreto",
+];
+
+function onClickButton(){
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+    const inputPrice = document.getElementById("inputPrecio");
+    precio = inputPrice.value;
+    let descuento;
+
+    switch(couponValue) {
+        case coupons[0]: // "JuanDC_es_Batman"
+        descuento = 15;
+        break;
+        case coupons[1]: // "pero_no_le_digas_a_nadie"
+        descuento = 30;
+        break;
+        case coupons[2]: // "es_un_secreto"
+        descuento = 25;
+        break;
+    }
+    const precioDescuento = calcularPrecioConDescuento(precio,descuento);
+    const precioFinal = document.getElementById("precioFinal");
+    precioFinal.innerText="El precio final es : "+precioDescuento;
+
+}
