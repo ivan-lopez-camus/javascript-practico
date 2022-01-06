bc41bf1ece7263b7485328c4c89ef0407b02c9b6
Ivan López <ivan.lopez1@unmsm.edu.pe>
2022-01-05 22:42:12 -0500

    modificar estilos de descuentos.html

--- descuentos/descuentos.js
+++ descuentos/descuentos.js
@@ -28,9 +28,9 @@ function onClickButtonPrice(){
 }
 
 const coupons = [
-    "JuanDC_es_Batman",
-    "pero_no_le_digas_a_nadie",
-    "es_un_secreto",
+    "Cupon1",
+    "Cupon2",
+    "Cupon3",
 ];
 
 function onClickButton(){
@@ -41,13 +41,13 @@ function onClickButton(){
     let descuento;
 
     switch(couponValue) {
-        case coupons[0]: // "JuanDC_es_Batman"
+        case coupons[0]:
         descuento = 15;
         break;
-        case coupons[1]: // "pero_no_le_digas_a_nadie"
+        case coupons[1]: 
         descuento = 30;
         break;
-        case coupons[2]: // "es_un_secreto"
+        case coupons[2]: 
         descuento = 25;
         break;
     }
